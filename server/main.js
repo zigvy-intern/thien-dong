648edd14e1d47c6e2983cc5fdcee02098c2cab85
unknown <dongkg3876@gmail.com>
2017-10-05 15:19:55 +0700

    Add Server and files

--- server/main.js
+++ server/main.js
@@ -3,3 +3,8 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+// import publications from './publications';
+// import methods from '.methods';
+
+// publications ();
+//methods ();
