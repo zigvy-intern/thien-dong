1f5fc69e5fee90ef7d582056a4c24b94c4a2dbd7
thientran511 <32450660+thientran511@users.noreply.github.com>
2017-10-05 14:46:07 +0700

    demo create PR

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+// commit demo
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
