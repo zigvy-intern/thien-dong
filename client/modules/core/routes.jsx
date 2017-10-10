480848c36ead97512751e1bbc5d70c628b38f822
unknown <dongkg3876@gmail.com>
2017-10-10 17:09:53 +0700

    chinh sua file Jsx,Js va testing chay thu

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -3,15 +3,22 @@ import {mount} from 'react-mounter';
 import Layout from './components/MainLayout.jsx';
 import ItemList from '../items/components/ItemList.jsx';
 import EditItem from '../items/components/EditItem.jsx';
-export default function (injectDeps, {
-FlowRouter }) {
+export default function (injectDeps, {FlowRouter }) {
   const MainLayoutCtx = injectDeps(Layout);
+  FlowRouter.route('/',{
+    name:'item.list',
+    action() {
+      mount(Layout,{
+        content:() => (<ItemList/ >)
+      })
+    }
+  });
   FlowRouter.route('/edit', {
     name: 'items.edit',
     action() {
       mount(MainLayoutCtx, {
-        content: () => (<EditItem />)
-        });
+        content: () => (<EditItem/ >)
+        })
     }
   });
  }
--- client/modules/items/components/Item.jsx
+++ client/modules/items/components/Item.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Col, Panel } from 'react-bootstrap';
-
+import { Row, Col, Panel, Glyphicon } from 'react-bootstrap';
 const Item = ({content}) => (
   <Col xs={4}>
     <Panel>
-      <h2>Việc Cần Làm</h2>
-      <p>Những thứ cần phải hoàn thành. </p>
+      <Row>
+        <Col xs={10}>
+          <a href="/"><Glyphicon glyph="chevron-left"></Glyphicon> Back to Items</a>
+          <h2>Thing to do</h2>
+          </Col>
+          <Col xs={2}>
+          <a href="/edit"><Glyphicon glyph="pencil"></Glyphicon></a>
+          </Col>
+      </Row>
+        <p>AAAAAAA</p>
     </Panel>
   </Col>
 );
