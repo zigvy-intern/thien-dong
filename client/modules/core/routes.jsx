31ca826e1587c893c1635341c6950769e87f5a88
unknown <dongkg3876@gmail.com>
2017-10-11 14:46:19 +0700

    fixed file routes.jsx

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -1,24 +1,49 @@
+import EditItem from '../items/components/EditItem.jsx';
 import React from 'react';
 import {mount} from 'react-mounter';
 import Layout from './components/MainLayout.jsx';
 import ItemList from '../items/components/ItemList.jsx';
-import EditItem from '../items/components/EditItem.jsx';
-export default function (injectDeps, {FlowRouter }) {
+
+export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(Layout);
-  FlowRouter.route('/',{
-    name:'item.list',
+
+  FlowRouter.route('/', {
+    name: 'items.list',
     action() {
-      mount(Layout,{
-        content:() => (<ItemList/ >)
+      mount(Layout, {
+        content: () => (<ItemList/ >)
       })
     }
   });
+
   FlowRouter.route('/edit', {
     name: 'items.edit',
     action() {
       mount(MainLayoutCtx, {
         content: () => (<EditItem/ >)
-        })
+      })
     }
   });
- }
+}
+
+/**
+export default function (injectDeps, {FlowRouter}) {
+  const MainLayoutCtx = injectDeps(Layout);
+  FlowRouter.route('/', {
+    name: 'items.list',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<p>Hello</p>)
+        });
+      }
+    });
+  }
+  FlowRouter.route('/edit', {
+    name: 'items.edit',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<EditItem />)
+        });
+      }
+  });
+  **/
