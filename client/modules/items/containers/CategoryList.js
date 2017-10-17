f0ceb1bc1d10788c18add294717ba7f4b841d1b9
thientran511 <32450660+thientran511@users.noreply.github.com>
2017-10-17 17:43:08 +0700

    update login

--- client/modules/items/containers/CategoryList.js
+++ client/modules/items/containers/CategoryList.js
@@ -1,13 +1,15 @@
 import CategoryList from '../components/CategoryList.jsx';
 import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
+
 export const composer = ({context}, onData) => {
-const {Meteor, Collections} = context();
-if (Meteor.subscribe('categories.list').ready()) {
-const categories = Collections.Categories.find().fetch();
-onData(null, {categories});
-}
+  const {Meteor, Collections} = context();
+  if (Meteor.subscribe('categories.list').ready()) {
+    const categories = Collections.Categories.find().fetch();
+    onData(null, {categories});
+  }
 };
+
 export default composeAll(
-composeWithTracker(composer),
-useDeps()
+  composeWithTracker(composer),
+  useDeps()
 )(CategoryList);
